96e74337a78b796c622a47a54948808086715e7d
Qamil-Mirza <qamilmirza@gmail.com>
2024-08-05 17:53:32 +0800

    fix vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
-// Get credentials from .env file
 const path = require('path');
 require('dotenv').config({
-    path: path.resolve(__dirname, '../.env')
+    path: path.resolve(__dirname, '.env')
 });
 
 const express = require('express');
@@ -9,19 +8,25 @@ const cors = require('cors');
 const oneUtamaRestaurantData = require('./scraper/restaurant_data.json');
 
 const app = express();
-const server_port = process.env.SERVER_PORT;
+const server_port = process.env.SERVER_PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/api/one-utama/restaurants', async (req, res) => {
+app.get('/api/one-utama/restaurants', (req, res) => {
     try {
         res.json(oneUtamaRestaurantData);
     } catch (error) {
         console.error(error);
+        res.status(500).send('Server error');
     }
-})
+});
 
-app.listen(server_port, () => console.log(`Server running on port ${server_port}`));
+// Listen (only if not in Vercel serverless environment)
+if (!process.env.VERCEL) {
+    app.listen(server_port, () => console.log(`Server running on port ${server_port}`));
+}
+
+module.exports = app;
